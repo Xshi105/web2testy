362e82c3b5815b3f50d51cf7a0d351d1c2f239a7
Xshi105 <31521348+Xshi105@users.noreply.github.com>
2017-09-13 23:34:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 color_list = ["black","white","red","blue","grey"]; 
-   "I like" + " " + 
-     color_list[Math.floor(Math.random()*color_list.length)] + "," + "and she likes " + " " + 
-     color_list[Math.floor(Math.random()*color_list.length)] + "."
-color_list = ["black","white","red","blue","grey"]
-let color = prompt("what color do you like?")
+let sentence1="I like" + " " +
+     color_list[Math.floor(Math.random()*color_list.length)] + ", "
 
-"You like" + " " + color + "," + " " + "and she likes " + " " + 
-  color_list[Math.floor(Math.random()*color_list.length)] + "."
+let sentence2="and she likes" + " " + 
+     color_list[Math.floor(Math.random()*color_list.length)] + ". "
+
+let word = prompt("what is your favorite color?")
+
+let sentence3="Although your favorite color is"+" "+word+", "+"the most beautiful color is aways a combination"+"."
+
+document.write(sentence1+sentence2+sentence3)
