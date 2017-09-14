da544abd9417ac43fbcc9393d78d219d635e5b92
Xshi105 <31521348+Xshi105@users.noreply.github.com>
2017-09-13 23:05:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ color_list = ["black","white","red","blue","grey"];
    "I like" + " " + 
      color_list[Math.floor(Math.random()*color_list.length)] + "," + "and she likes " + " " + 
      color_list[Math.floor(Math.random()*color_list.length)] + "."
-color_lisk = ["black","white","red","blue","grey"]
+color_list = ["black","white","red","blue","grey"]
 let color = prompt("what color do you like?")
 
 "You like" + " " + color + "," + " " + "and she likes " + " " + 
